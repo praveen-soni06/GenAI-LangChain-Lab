bb811d16a08393c64b408ef9c5328a3a00580d7c
praveen-soni06 <praveenkumar12032@gmail.com>
2026-04-26 19:02:45 +0530

    runnable passthrough

---
+++ Runnables/runnable_passthrough.py
@@ -0,0 +1,35 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id='Qwen/Qwen2.5-72B-Instruct',
+                          task='text-generation',
+                          max_new_tokens=512)
+
+model = ChatHuggingFace(llm = llm)
+
+parser = StrOutputParser()
+
+
+prompt = PromptTemplate(template='make a joke about the {topic}',
+                        input_variables=['topic'])
+
+prompt2 = PromptTemplate(template='explain the following joke - {text}',
+                         input_variables=['text'])
+
+joke_gen_chain = RunnableSequence(prompt, model, parser)
+
+parallel_chain = RunnableParallel({'joke': RunnablePassthrough(),
+                                      'explaination': RunnableSequence(prompt2, model, parser)})
+
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+result = final_chain.invoke({'topic': 'GenAI'})
+
+print('Joke : \n',result['joke'])
+print('\n\nExplaination : \n', result['explaination'])
